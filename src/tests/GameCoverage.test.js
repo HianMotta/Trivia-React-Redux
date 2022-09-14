18ba42030556b4fcb2ce42fbb50e5fd20ca7ff81
Marcelo Ferreira <marceloferreira.ihu72@gmail.com>
2022-09-14 14:57:22 -0300

    requisito 21 finalizado

--- src/tests/GameCoverage.test.js
+++ src/tests/GameCoverage.test.js
@@ -6,7 +6,6 @@ import App from '../App'
 import {falseData, data} from './mocks/mockQuestionsAPI'
 
 const mockToken = '00cb469ce38726ee00a7c6836761b0a4fb808181a125dcde6d50a9f3c9127b6';
-const imgUrl = `https://www.gravatar.com/avatar/${mockToken}`
 
 const standardBeforeEach = () => renderWithRouterAndRedux(
   <App />,
